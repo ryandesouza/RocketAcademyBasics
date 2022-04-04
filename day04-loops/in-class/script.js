6b9225730b995c575904794d82f63ac856f2b497
ryandesouza <44060153+ryandesouza@users.noreply.github.com>
2022-04-04 21:49:05 +0800

    Added project

--- day04-loops/in-class/script.js
+++ day04-loops/in-class/script.js
@@ -1,24 +1,106 @@
 var emojiNumberCharactersMain = function (input) {
-  // Complete the Base: Emoji Drawing Number of Characters exercise below with emojiNumberCharactersMain as the main function.
-  var myOutputValue = 'hello world';
+  var counter = 0; 
+  var myOutputValue = '';
+  while (counter < input){
+    myOutputValue = myOutputValue + '👍🏻';
+    counter=counter+1;
+  }
   return myOutputValue;
 };
 
 var emojiSquareMain = function (input) {
   // Complete the Base: Emoji Drawing Square exercise below with emojiSquareMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = '';
+  
+  // Initialise the outer counter, rowCounter
+  var rowCounter = 0;
+  
+  while (rowCounter < input) {
+  
+      // Inside the outer loop, initialise the inner counter, columnCounter
+    var columnCounter = 0;
+    
+    // Every time the outer loop runs, the inner loop runs repeatedly until
+    // the inner loop condition is met.
+    while (columnCounter < input) {
+    
+      // Each time the inner loop runs, it adds "x" to output
+      myOutputValue = myOutputValue + '👍🏻';
+      columnCounter = columnCounter + 1;
+    }
+    
+    // At the end of each outer loop, add a <br> tag to begin a new row
+    myOutputValue = myOutputValue + '<br>';
+    rowCounter = rowCounter + 1;
+  }
+  
+  // After the outer loop has run to completion, return the output compiled
+  // by the above loops.
   return myOutputValue;
 };
 
+
+
+
 var emojiTriangleMain = function (input) {
   // Complete the Comfortable: Emoji Drawing Triangle exercise below with emojiTriangleMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = '';
+  
+  // Initialise the outer counter, rowCounter
+  var rowCounter = 0;
+  
+  while (rowCounter < input) {
+  console.log(`Row counter = ${rowCounter}`);
+      // Inside the outer loop, initialise the inner counter, columnCounter
+    var columnCounter = 0;
+    
+    // Every time the outer loop runs, the inner loop runs repeatedly until
+    // the inner loop condition is met.
+    while (columnCounter <= rowCounter) {
+      console.log(`Column counter = ${columnCounter}`);
+      // Each time the inner loop runs, it adds "x" to output
+      myOutputValue = myOutputValue + '👍🏻';
+      columnCounter = columnCounter + 1;
+    }
+    
+    // At the end of each outer loop, add a <br> tag to begin a new row
+    myOutputValue = myOutputValue + '<br>';
+    rowCounter = rowCounter + 1;
+  }
+  
+  // After the outer loop has run to completion, return the output compiled
+  // by the above loops.
   return myOutputValue;
 };
 
 var emojiOutlineSquareMain = function (input) {
   // Complete the More Comfortable: Emoji Drawing Outline Square exercise below with emojiOutlineSquareMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = '';
+  
+  // Initialise the outer counter, rowCounter
+  var rowCounter = 0;
+  
+  while (rowCounter < input) {
+  
+      // Inside the outer loop, initialise the inner counter, columnCounter
+    var columnCounter = 0;
+    
+    // Every time the outer loop runs, the inner loop runs repeatedly until
+    // the inner loop condition is met.
+    while (columnCounter < input) {
+    
+      // Each time the inner loop runs, it adds "x" to output
+      myOutputValue = myOutputValue + '👍🏻';
+      columnCounter = columnCounter + 1;
+    }
+    
+    // At the end of each outer loop, add a <br> tag to begin a new row
+    myOutputValue = myOutputValue + '<br>';
+    rowCounter = rowCounter + 1;
+  }
+  
+  // After the outer loop has run to completion, return the output compiled
+  // by the above loops.
   return myOutputValue;
 };
 
